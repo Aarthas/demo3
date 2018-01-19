2841d8db6a876f563cb45e81c6ece01c9bf23d81
shenyibin <497721363@qq.com>
2018-01-19 10:54:17 +0800

    add servlet support

--- src/main/java/com/example/demo3/Demo3Application.java
+++ src/main/java/com/example/demo3/Demo3Application.java
@@ -2,12 +2,14 @@ package com.example.demo3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
 
 @SpringBootApplication
+@ServletComponentScan
 public class Demo3Application extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
